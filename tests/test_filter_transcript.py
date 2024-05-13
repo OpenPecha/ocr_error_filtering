99a1a9776da5d7f252357a3069a1da21dd5bfeed
gangagyatso4364 <gyatsokhan@gmail.com>
2024-05-13 13:04:21 +0530

    added test case

--- tests/test_filter_transcript.py
+++ tests/test_filter_transcript.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from ocr_error_filtering.filter_transcript import update_csv_files
 
 
@@ -5,15 +7,8 @@ def test_update_csv_files():
     transcript_dir = "tests/test_data/transcript"
     page_ids = ["I1KG155250419", "I3CN46890016"]
     save_error_transcript_path = "tests/test_data/error_transcript.csv"
-    page_id_to_batch = update_csv_files(
-        transcript_dir, page_ids, save_error_transcript_path
-    )
-    expected_page_id_to_batch = {
-        "I3CN46890016": ["batch_1"],
-        "I1KG155250419": ["batch_2"],
-    }
-    assert page_id_to_batch == expected_page_id_to_batch
-
-
-if __name__ == "__main__":
-    test_update_csv_files()
+    update_csv_files(transcript_dir, page_ids, save_error_transcript_path)
+    # Check that the error data was saved correctly
+    error_data = pd.read_csv(save_error_transcript_path)
+    expected_error_data = pd.read_csv("tests/test_data/expected_error_transcript.csv")
+    pd.testing.assert_frame_equal(error_data, expected_error_data)
