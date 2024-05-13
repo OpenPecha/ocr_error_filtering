3aae14347e5a61b97cab6a0d1de60a7995f10862
gangagyatso4364 <gyatsokhan@gmail.com>
2024-05-13 10:47:51 +0530

    added test case

--- tests/test_filter_transcript.py
+++ tests/test_filter_transcript.py
@@ -5,8 +5,7 @@ def test_update_csv_files():
     page_ids = ['I1KG155250419', 'I3CN46890016']
     save_error_transcript_path = "./tests/test_data/error_transcript.csv"
     page_id_to_batch = update_csv_files(transcript_dir, page_ids, save_error_transcript_path)
-    print(page_id_to_batch)
-    assert page_id_to_batch == {'I3CN46890016': ['batch_1'], 'I1KG155250419': ['batch_2']}
+    expected_page_id_to_batch = {'I3CN46890016': ['batch_1'], 'I1KG155250419': ['batch_2']}
+    assert page_id_to_batch == expected_page_id_to_batch
 
-if __name__ == '__main__':
-    test_update_csv_files()
+
